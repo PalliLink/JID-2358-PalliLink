77532c11a6e6850492bb2b38a6ddcfdc1144a262
Jason <88212653+raw-chicken@users.noreply.github.com>
2023-04-03 17:26:44 -0400

    Dehardcode retrievePatientProfile (#62)
    * dehardcode retrievePatientProfile
    
    * remove generalPractitioner
    
    * Make nullable

--- lib/models/name_model.dart
+++ lib/models/name_model.dart
@@ -1,10 +1,15 @@
 class Name {
-  final String family;
-  final String given;
-  final String prefix = "";
-  final String suffix = "";
-  final String text;
-  final String use;
+  late String family;
+  late String given;
+  String prefix = "";
+  String suffix = "";
+  late String text;
 
-  Name(this.family, this.given, this.text, this.use);
+  Name(this.family, this.given, this.text);
+
+  Name.fromJson(Map<dynamic, dynamic> json) {
+    family = json['family'];
+    given = json['given'];
+    text = json['text'];
+  }
 }
--- lib/views/patient/patient_home.dart
+++ lib/views/patient/patient_home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:pallinet/components/custom_button.dart';
+import 'package:pallinet/components/loading.dart';
 import 'package:pallinet/components/pain_diary.dart';
 import 'package:pallinet/firestore/firestore.dart';
 import 'package:pallinet/models/patient_model.dart';
+import 'package:pallinet/models/physician_model.dart';
 import 'package:pallinet/models/session_manager.dart';
 
 class PatientHome extends StatefulWidget {
@@ -24,65 +26,67 @@ class _PatientHomeState extends State<PatientHome> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<PatientID?>(
+    return FutureBuilder<Patient?>(
       future: _prefs.getUid().then((uid) => retrievePatientProfile(uid)),
       builder: ((context, snapshot) {
         Widget gap() => const SizedBox(height: 30);
-
-        String patientName = snapshot.data == null
-            ? "Loading..."
-            : (snapshot.data as PatientID).name;
-        return Scaffold(
-          appBar: AppBar(title: Text(patientName)),
-          body: ListView(
-            padding: const EdgeInsets.all(15),
-            children: [
-              const CustomButton(
-                icon: Icons.calendar_month_rounded,
-                iconColor: Color.fromRGBO(64, 192, 251, 1),
-                route: '/patient/calendar',
-                text: 'Calendar',
-              ),
-              gap(),
-              const SizedBox(height: 300, child: PainDiary()),
-              gap(),
-              const CustomButton(
-                icon: Icons.schedule,
-                iconColor: Color.fromRGBO(64, 192, 251, 1),
-                route: '/patient/appointments',
-                text: 'Appointments',
-              ),
-              gap(),
-              const CustomButton(
-                icon: Icons.health_and_safety_outlined,
-                iconColor: Colors.red,
-                route: '/patient/treatments',
-                text: 'Treatments',
-              ),
-              gap(),
-              const CustomButton(
-                icon: Icons.medication,
-                iconColor: Colors.purpleAccent,
-                route: '/patient/medications',
-                text: 'Medications',
-              ),
-              gap(),
-              const CustomButton(
-                icon: Icons.local_hospital,
-                iconColor: Colors.green,
-                route: '/patient/recommendedspecialists',
-                text: 'Recommended Specialists',
-              ),
-              gap(),
-              const CustomButton(
-                icon: Icons.list_alt_sharp,
-                iconColor: Colors.redAccent,
-                route: '/patient/endoflifeplans',
-                text: 'End of Life Plans',
-              ),
-            ],
-          ),
-        );
+        if (snapshot.hasData) {
+          Patient? patientData = snapshot.data;
+          String patientName = patientData!.name.text;
+          return Scaffold(
+            appBar: AppBar(title: Text(patientName)),
+            body: ListView(
+              padding: const EdgeInsets.all(15),
+              children: [
+                const CustomButton(
+                  icon: Icons.calendar_month_rounded,
+                  iconColor: Color.fromRGBO(64, 192, 251, 1),
+                  route: '/patient/calendar',
+                  text: 'Calendar',
+                ),
+                gap(),
+                const SizedBox(height: 300, child: PainDiary()),
+                gap(),
+                const CustomButton(
+                  icon: Icons.schedule,
+                  iconColor: Color.fromRGBO(64, 192, 251, 1),
+                  route: '/patient/appointments',
+                  text: 'Appointments',
+                ),
+                gap(),
+                const CustomButton(
+                  icon: Icons.health_and_safety_outlined,
+                  iconColor: Colors.red,
+                  route: '/patient/treatments',
+                  text: 'Treatments',
+                ),
+                gap(),
+                const CustomButton(
+                  icon: Icons.medication,
+                  iconColor: Colors.purpleAccent,
+                  route: '/patient/medications',
+                  text: 'Medications',
+                ),
+                gap(),
+                const CustomButton(
+                  icon: Icons.local_hospital,
+                  iconColor: Colors.green,
+                  route: '/patient/recommendedspecialists',
+                  text: 'Recommended Specialists',
+                ),
+                gap(),
+                const CustomButton(
+                  icon: Icons.list_alt_sharp,
+                  iconColor: Colors.redAccent,
+                  route: '/patient/endoflifeplans',
+                  text: 'End of Life Plans',
+                ),
+              ],
+            ),
+          );
+        } else {
+          return const LoadingScreen("Loading Home");
+        }
       }),
     );
   }
