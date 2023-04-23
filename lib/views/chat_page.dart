0df1080f3b689325c05e67e01c89516f2e4f0b9e
Khoang4 <kenny.m.hoang@gmail.com>
2023-04-23 18:58:47 -0400

    setting up chat page

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:pallinet/components/appointment_card.dart';
+import 'package:pallinet/models/session_manager.dart';
+import 'package:pallinet/firestore/firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pallinet/utils.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late final SessionManager _prefs;
+
+  @override
+  void initState() {
+    _prefs = SessionManager();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        children: const [
-          Text("ChatPage"),
-        ],
+    return Column(children: [
+      Expanded(
+        flex: 8,
+        child: FutureBuilder<List<dynamic>>(
+          future:
+              _prefs.getUid().then((uid) => retrieveAppointmentsPatients(uid)),
+          builder: ((context, snapshot) {
+            final list = snapshot.data == null
+                ? []
+                : (snapshot.data as List)
+                    .map((e) => e as Map<String, dynamic>)
+                    .toList();
+            return Scaffold(
+              appBar: AppBar(title: const Text("Appointments")),
+              body: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final data = list[index];
+                  Timestamp t = data["scheduledTimeStart"] as Timestamp;
+                  DateTime startTime = t.toDate();
+                  return AppointmentCard(
+                    name: data["practitioner"],
+                    date: startTime,
+                    appointmentType: data["appointmentType"],
+                    id: data["appointmentID"],
+                    refresh: () => refresh(setState),
+                  );
+                },
+              ),
+            );
+          }),
+        ),
       ),
-    ));
+    ]);
   }
 }
